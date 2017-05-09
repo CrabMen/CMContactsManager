250d411adb41218dd6be2d7e38de40101835c002
guoqiang <tobecrabman@163.com>
2017-05-09 18:56:59 +0800

    项目初始化通讯录权限

---
+++ CMContactsManager/CMContactsManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CMContactsManager
+//
+//  Created by CrabMan on 2017/5/9.
+//  Copyright © 2017年 CrabMan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CMContactsManager/CMContactsManager/CMContactsManager.m
@@ -0,0 +1,62 @@
+//
+//  CMContactsManager.m
+//  CMContactsManager
+//
+//  Created by CrabMan on 2017/5/9.
+//  Copyright © 2017年 CrabMan. All rights reserved.
+//
+
+#import "CMContactsManager.h"
+#import <UIKit/UIKit.h>
+#import <Contacts/Contacts.h>
+
+@implementation CMContactsManager
+
++ (instancetype)shareInstance {
+  __block  CMContactsManager *manager = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        manager = [[CMContactsManager alloc]init];
+    });
+    
+    return manager;
+}
+
+
+/**检查用户授权状态*/
+- (void)checkUserAuthorization {
+
+    CNAuthorizationStatus status = [CNContactStore authorizationStatusForEntityType:CNEntityTypeContacts];
+    
+    if (status == CNAuthorizationStatusNotDetermined) {
+        
+        
+        
+        
+    } else if (status == CNAuthorizationStatusAuthorized) {
+        
+    
+    } else if (status == CNAuthorizationStatusDenied) {
+    
+    
+      NSURL *url = [NSURL URLWithString:UIApplicationOpenSettingsURLString];
+        [[UIApplication sharedApplication]openURL:url];
+        
+    }
+    
+    
+
+}
+
+//- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+//{
+//
+//
+//
+//}
+
+
+
+
+
+@end
